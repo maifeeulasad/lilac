cc0513372311f75bc3b2285e7dd3d2c590aef7a2
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-02 12:01:18 +0600

    [vite,config]: vite config for ui;

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: mode === 'lib' ? {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'LilacEditor',
+      formats: ['es', 'umd'],
+      fileName: (format) => `lilac-editor.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  } : {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    // open: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+}))
