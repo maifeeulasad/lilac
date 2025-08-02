40c3ad3ff54cfc3eb41cb75acc89ae7da83b1f58
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-02 15:49:05 +0600

    [export]: added types for plugin;

---
+++ src/plugins/index.ts
@@ -0,0 +1,8 @@
+// Plugin Manager
+export { PluginManager, pluginManager } from './PluginManager';
+
+// Built-in Plugins
+export { emojiPlugin } from './emojiPicker';
+
+// Plugin Types
+export * from '../types/plugin';
