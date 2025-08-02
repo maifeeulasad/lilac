8fc45e0aa468625c0dc2994860c476c61cd8f669
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-02 16:02:49 +0600

    [export]: exported table and word-count plugin;

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,7 +2,9 @@
 export { PluginManager, pluginManager } from './PluginManager';
 
 // Built-in Plugins
+export { wordCountPlugin } from './wordCount';
 export { emojiPlugin } from './emojiPicker';
+export { tablePlugin } from './tableInserter';
 
 // Plugin Types
 export * from '../types/plugin';
