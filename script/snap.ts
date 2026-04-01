77e7a2dd51166eb9ce1d7a934ef73442869cb0c1
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-02 02:35:31 +0600

    [refactor]: add delay for lazy loading in screenshot script;

--- script/snap.ts
+++ script/snap.ts
@@ -3,6 +3,8 @@ const { chromium } = require('playwright');
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
+    // delay 3 seconds for lazy loading for editor
+    await page.waitForTimeout(3000);
     await page.screenshot({ path: 'snap/screenshot.png' });
     await browser.close();
 })()
