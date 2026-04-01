b3507d66e36afb75f8a54b0c94d42b29e0a10895
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-02 00:10:28 +0600

    [script]: removed route from snap taking url;

--- script/snap.ts
+++ script/snap.ts
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 (async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000/lilac/', { waitUntil: 'networkidle' });
+    await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
     await page.screenshot({ path: 'snap/screenshot.png' });
     await browser.close();
 })()
