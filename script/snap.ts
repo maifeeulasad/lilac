0325625d3a813ff30e031a472270b9b8f9364643
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-02 02:59:05 +0600

    [refactor]: delay after editor visibility;

--- script/snap.ts
+++ script/snap.ts
@@ -3,10 +3,10 @@ const { chromium } = require('playwright');
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
-    // delay 3 seconds for lazy loading for editor
-    await page.waitForTimeout(3000);
     // wait for id "editor-root" to be visible
     await page.waitForSelector('#editor-root', { state: 'visible' });
+    // delay 2 seconds for lazy loading for editor
+    await page.waitForTimeout(2000);
     await page.screenshot({ path: 'snap/screenshot.png' });
     await browser.close();
 })()
