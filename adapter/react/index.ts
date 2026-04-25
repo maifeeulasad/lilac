c579eff1010e18ffdc20ed18ec1e0ec6cb97c36f
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-25 14:04:41 +0600

    [fix]: changed core import for react;

--- adapter/react/index.ts
+++ adapter/react/index.ts
@@ -1,6 +1,6 @@
 // Lilac React Adapter
 // Provides React bindings for the Lilac WYSIWYG editor
 
-export * from '../core/index';
+export * from 'core';
 export { createReactAdapter } from './adapter';
 
