783dd1b1eb0bbd43e96769e6e913b16b7f32fefb
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-25 15:37:23 +0600

    [version]: set to `0.4.0` for `core/plugins/emojiPicker.ts`

--- core/plugins/emojiPicker.ts
+++ core/plugins/emojiPicker.ts
@@ -27,7 +27,7 @@ const EMOJI_CATEGORIES = {
 export const emojiPlugin: EditorPlugin = {
   id: 'emoji-picker',
   name: 'Emoji Picker',
-  version: '0.3.2',
+  version: '0.4.0',
   description: 'Add emojis to your content with an easy-to-use picker',
   author: 'Lilac Editor',
 
